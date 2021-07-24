b3ab85e30573c2828268211eaa139f1543403e43
Vipul Nair <vipulnair@PINEMAC0034.local>
2021-07-24 23:51:48 +0530

    port change

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+const port = process.env.PORT
+
+app.listen(port, () => console.log('listening at 30q0'));
 
 
-app.listen(3000, () => console.log('listening at 3000'));
+
 app.use(express.static('public'));
 app.post('/api', (request, response) => {
 

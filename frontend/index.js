94c9509b5558d52c1bb97e21799a841555746d5b
Vipul Nair <vipulnair@PINEMAC0034.local>
2021-07-24 23:38:36 +0530

    first commit for dummy merchant website

---
+++ frontend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.post('/api', (request, response) => {
+
+    console.log(request);
+})
